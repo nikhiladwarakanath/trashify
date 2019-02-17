15156b60a9e1f9b8c2af9fb8a9f0695fa6f99c5e
Kavya9294 <kavya.ub9294@gmail.com>
2019-02-16 21:11:36 -0500

    Made API calls to server to fetch required data and display.

--- server/nav/index.js
+++ server/nav/index.js
@@ -4,6 +4,25 @@ const fs = require('fs');
 
 const router = express.Router();
 
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 module.exports = () => {
     router.get('/', ( _req, res, next) => {
         console.log("start of server");
@@ -16,22 +35,22 @@ module.exports = () => {
   router.get('/closeBins', (req,res,next) => {
     let rawData = fs.readFileSync('./data/data.json')
     var allBins = JSON.parse(rawData)
-    var curLocLong =73.944158
-    var curLocLat = 40.678178
+    var curLocLat =req.query.latitude
+    var curLocLong = req.query.longitude
     var distance = [];
     var fullResponse = {};
     for(i=0;i<allBins.length;i++){
       var curValue = allBins[i]
       var lat = curValue["Latitude"]
       var long = curValue["Longitude"]
-      distance[i] = geolib.getDistance(
-                    {"latitude": curLocLat, "longitude":curLocLong},
-                    {"latitude": lat, "longitude": long}
-                    );
+      distance[i] = getDistanceFromLatLonInKm(
+                    curLocLat,curLocLong,
+                    lat, long);
       var k = distance[i]
       fullResponse[k]={"Site type":curValue["Site type"],"latitude":lat,"longitude":long}
     }
-    var top5 = Object.keys(fullResponse).sort()
+    var top5 = Object.keys(fullResponse)
+    top5 = top5.sort()
     top5 = top5.slice(0,5)
     var resp=[]
     for(i=0;i<5;i++){
